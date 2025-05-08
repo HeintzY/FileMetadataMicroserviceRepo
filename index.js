3da11524a879598b91a1457a563cc9899ddaa337
HeintzY <heintzymail@testmail.com>
2025-05-08 08:55:07 +0200

    feat: add finished project

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ app.get('/', function (req, res) {
 });
 
 
+// Project solution:
+const multer = require("multer")
+app.post("/api/fileanalyse", multer().single("upfile"), function (req, res) {
+  res.json({ name: req.file.originalname, type: req.file.mimetype, size: req.file.size });
+});
 
 
 const port = process.env.PORT || 3000;
